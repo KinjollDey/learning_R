14dea5cb6ea863f62b2301a3c1137c7f300ec1ca
KinjollDey <kinjolldey@gmail.com>
2019-09-11 06:18:25 -0700

    Started Set 2 & updated objective

--- Practice-Set2/script.R
+++ Practice-Set2/script.R
@@ -1,9 +1,9 @@
 ## Practice Set 2
 
 ## calling in built data
-data(iris)
-iris
-plot(iris)
+#data(iris)
+#iris
+#plot(iris)
 
 
 ## Set Working directory
@@ -12,9 +12,12 @@ setwd("C:\\Users\\user 1\\Desktop\\R\\learning_R\\Practice-Set2 ")
 getwd()
 
 ## Save and call dataset
-write.csv(iris, "C:\\Users\\user 1\\Desktop\\R\\learning_R\\Practice-Set2\\data.csv")
+#write.csv(iris, "C:\\Users\\user 1\\Desktop\\R\\learning_R\\Practice-Set2\\data.csv")
+
+
 data<- read.csv("C:\\Users\\user 1\\Desktop\\R\\learning_R\\Practice-Set2\\data.csv", header = T, stringsAsFactors = F)
 View(data)
+summary(data)
 
 ## Loading Packages required
 #pkgs <- c('reshape2', 'plyr', 'ggplot2', 'dplyr', 'data.table', 'Lahman')
@@ -22,6 +25,7 @@ View(data)
 
 ## Data manipulations
 attach(data)
+
 apply(data, MARGIN = 2, max)
 lapply(data, max)
 sapply(data, max)
@@ -30,8 +34,32 @@ average <- function(x){
   max(x)+ min(x)/ 2}
 sapply(data, average)
 
+##Random Sampling
+random_samp <- iris[sample(nrow(iris),10),]
+summary(random_samp)
+apply(random_samp, MARGIN = 2, max)
+
+## Suppose we want to see all the data from a particular species
+## Say we use the species 'setosa'
+
+
+## From complete Data Set
+setosa <- subset(data, Species== 'setosa', select = c(1:5))
+head(setosa)
+
+subset(data, Species== 'setosa' & Sepal.Length > 3)
+
+
+## Plots(Basic)
+plot(data$Petal.Length, data$Petal.Width, main="Iris Data")
+plot(data$sepal.width, data$sepal.length, xlim = 5, ylim = 5)
+hist(data$Sepal.Length)
+
+plot(data)
+## Plots (Advanced)
+
+
+
 ##loops 
 ## Apply
 ## statistics
-## Plots(Basic)
-## Plots (Advanced)
--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-## learning_R
-This repository contains a R script that operates showcasing the general built-in R functions and codes using a random datasets  
+# learning_R
+This repository contains a R script that operates showcasing the general built-in R functions and codes using a random datasets.  
 
-Practice set 1 : 
-### Used data set called Airport_data.
-### Used basis built in R functions to manipulate and operate on data.
-### Uploaded file to Google drive using API.  
+Practice set 1 : Basic Functions 
+
+#### Used data set called Airport_data.
+#### Used basis built in R functions to manipulate and operate on data.
+#### Uploaded file to Google drive using API.  
+
+Practice Set 2 : Graphs and Plots
+
+Practice Set 3 : Nucleotide count
+
+Practice Set 4 : Instrument Data
